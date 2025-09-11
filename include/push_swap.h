cce6031c11843495e0d13695562d97234eb658e4
Dennis <dennis.wt20@gmail.com>
2025-09-11 12:54:22 +0200

    add t_list

--- include/push_swap.h
+++ include/push_swap.h
@@ -6,16 +6,23 @@
 /*   By: dwotsche <dwotsche@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/11 11:39:51 by dwotsche          #+#    #+#             */
-/*   Updated: 2025/09/11 12:12:11 by dwotsche         ###   ########.fr       */
+/*   Updated: 2025/09/11 12:53:43 by dwotsche         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
 
+// stack init
+typedef struct s_list
+{
+	int				value;
+	int				index;
+	struct s_list	*next;
+}				t_list;
+
 // error handling
 void	ft_print_error(void);
-int		ft_is_args_correct(int argc, char **argv);
 
 # include "../lib/libft/include/libft.h"
 # include "../lib/printf/include/ft_printf.h"
