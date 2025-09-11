2af3cfef430a24ac0580a9f78d65e22042b7065d
Dennis <dennis.wt20@gmail.com>
2025-09-11 18:28:17 +0200

    add comments for stack_operations

---
+++ src/stack_operations.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   stack_operations.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dwotsche <dwotsche@student.42wolfsburg.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/11 12:54:32 by dwotsche          #+#    #+#             */
+/*   Updated: 2025/09/11 13:00:54 by dwotsche         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+// ----- swap
+
+// sa (swap a): Swap the first 2 elements at the top of stack a.
+// sb (swap b): Swap the first 2 elements at the top of stack b.
+// ss : sa and sb at the same time.
+
+// ----- push
+
+// pa (push a): Take the first element at the top of b and put it at the top of a.
+// pb (push b): Take the first element at the top of a and put it at the top of b.
+
+// ----- rotate
+
+// ra (rotate a): Shift up all elements of stack a by 1.
+// rb (rotate b): Shift up all elements of stack b by 1.
+// rr : ra and rb at the same time.
+
+// ----- reverse rotate
+
+// rra (reverse rotate a): Shift down all elements of stack a by 1.
+// rrb (reverse rotate b): Shift down all elements of stack b by 1.
+// rrr : rra and rrb at the same time.
