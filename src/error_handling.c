076450e08505c52636b6c7a51cfb261b4f95227b
Dennis <dennis.wt20@gmail.com>
2025-09-11 12:54:07 +0200

    add error msg

--- src/error_handling.c
+++ src/error_handling.c
@@ -6,7 +6,7 @@
 /*   By: dwotsche <dwotsche@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/11 11:55:00 by dwotsche          #+#    #+#             */
-/*   Updated: 2025/09/11 12:14:00 by dwotsche         ###   ########.fr       */
+/*   Updated: 2025/09/11 12:53:57 by dwotsche         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,6 @@
 
 void	ft_print_error(void)
 {
-	ft_putstr_fd("Error\n", 1);
-	exit(0);
-}
-
-int	ft_is_args_correct(int argc, char **argv)
-{
-	long	val;
-
-	val = 0;
-	if (argc != 2)
-		return (0);
-	val = ft_atoi(argv[1]);
-	return (val);
+	ft_putstr_fd("Error\n", 2);
+	exit(1);
 }
