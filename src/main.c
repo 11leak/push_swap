75254829afc091fefcbbfcfb0e7a5fc27edca1a7
Dennis <dennis.wt20@gmail.com>
2025-09-16 16:42:06 +0200

    finish main

--- src/main.c
+++ src/main.c
@@ -6,11 +6,24 @@
 /*   By: dwotsche <dwotsche@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/11 11:43:13 by dwotsche          #+#    #+#             */
-/*   Updated: 2025/09/15 15:01:50 by dwotsche         ###   ########.fr       */
+/*   Updated: 2025/09/16 15:14:05 by dwotsche         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+
+static void	sort(t_list **a, t_list **b)
+{
+	if (lst_size(*a) <= 5)
+	{
+		small_sort(a, b);
+	}
+	else
+	{
+		index_allocation(*a);
+		radix_sort(a, b);
+	}
+}
 
 int	main(int argc, char **argv)
 {
@@ -28,19 +41,7 @@ int	main(int argc, char **argv)
 		return (1);
 	}
 	if (!is_sorted(a))
-	{
-		if (lst_size(a) <= 5)
-		{
-			// tiny sort - hardcoded
-			return (0);
-		}
-		else
-		{
-			// radix sort
-			return (0);
-		}
-	}
-
+		sort(&a, &b);
 	lst_clear(&a);
 	lst_clear(&b);
 	return (0);
